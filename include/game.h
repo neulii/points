231dc7ea30a4250803a968c875399ea5de344a87
neulii <neulii@gmx.at>
2020-03-19 14:56:57 +0100

    asdf

---
+++ include/game.h
@@ -0,0 +1,29 @@
+#ifndef GAME_H
+#define GAME_H
+
+class Game
+{
+private:
+
+	bool gameRuns = false;
+	std::vector<Player*> players;
+
+
+
+public:
+	Game();
+
+	void startNewGame(std::vector<Player*> players);
+	void saveGame(std::string fileName);
+	void openGame(std::string fileName);
+	int checkIfPlayerHasWon();
+
+
+
+
+
+
+
+
+};
+#endif // !GAME_H
