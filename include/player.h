5af5f97286cf56e29a5fdce5950673925b96b654
neulii <neulii@gmx.at>
2020-03-22 11:16:19 +0100

    worked on player class

--- include/player.h
+++ include/player.h
@@ -1,5 +1,23 @@
-#pragma once
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <iostream>
+#include <vector>
+
 class Player
 {
+private:
+    
+    std::string name;
+    int actualPoints;
+    std::vector<int> pointList;
+    
+public:
+    
+    Player(std::string name);
+    
+    
 };
+
+#endif
 
--- src/player.cpp
+++ src/player.cpp
@@ -1,1 +1,7 @@
 #include "player.h"
+
+
+Player::Player(std::string name)
+    : name(name){
+    
+}
